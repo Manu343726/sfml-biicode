a94fc439ae12dfd7a4f7786fa1f015df20a2926c
Manu343726 <Manu343726@gmail.com>
2014-09-26 18:18:18 +0200

    dfasfesfesfeswfesfes

---
+++ build/manu343726/sfml/CMakeFiles/manu343726_sfml.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/manu343726_sfml.dir/cmake_dummy.cpp.o"
+  "libmanu343726_sfml.pdb"
+  "libmanu343726_sfml.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/manu343726_sfml.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/manu343726/sfml_blocktests/CMakeFiles/manu343726_sfml_blocktests_main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/manu343726_sfml_blocktests_main.dir/main.cpp.o"
+  "/home/manu343726/Documentos/sfml-biicode/bin/manu343726_sfml_blocktests_main.pdb"
+  "/home/manu343726/Documentos/sfml-biicode/bin/manu343726_sfml_blocktests_main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/manu343726_sfml_blocktests_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
