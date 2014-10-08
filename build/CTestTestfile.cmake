0b4aaddaa74cc89d2ada19f265e69ee85a2eab80
Manu343726 <Manu343726@gmail.com>
2014-10-08 16:27:25 +0200

    Windows ok. Copy dlls step magically working

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/manu343726/Documentos/sfml-biicode/cmake
-# Build directory: /home/manu343726/Documentos/sfml-biicode/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(manu343726/sfml_blocktests)
-subdirs(manu343726/sfml)
